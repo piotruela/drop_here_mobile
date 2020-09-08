550a161a8e75b82c1a248574bbdbd32cd5cee062
maciej.swierad <maciej.swierad@gmail.com>
2020-09-08 16:58:30 +0200

    edited splash_page

--- lib/counter/ui/pages/splash_page.dart
+++ lib/counter/ui/pages/splash_page.dart
@@ -15,7 +15,8 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(child: Scaffold(
+    return MainLayout(
+        child: Scaffold(
       backgroundColor: Colors.transparent,
       body: Center(
         child: Column(
@@ -23,38 +24,41 @@ class SplashPage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(top: 172.0),
               child: Container(
-                  height: 193,
-                  width: 268,
-                  child: SvgPicture.asset(assetsConfig.splashScreenImage),
+                height: 120,
+                child: SvgPicture.asset(assetsConfig.splashScreenImage),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 19.0, bottom: 17.0),
-              child: Text(
-                  Localization.of(context).bundle.appTitle, style: themeConfig.textStyles.primaryTitle),
+              child: Text(Localization.of(context).bundle.appTitle,
+                  style: themeConfig.textStyles.primaryTitle),
             ),
             Padding(
               padding: const EdgeInsets.only(left: 67.0, right: 67.0),
-              child: Text(Localization
-                  .of(context)
-                  .bundle
-                  .lorem, style: themeConfig.textStyles.contentTitle,
+              child: Text(Localization.of(context).bundle.lorem,
+                  style: themeConfig.textStyles.contentTitle,
                   textAlign: TextAlign.center),
             ),
-            Padding(//TODO: Use DhButton padding
+            Padding(
+                //TODO: Use DhButton padding
                 padding: const EdgeInsets.only(top: 55.0),
                 child: DhButton(
                   onPressed: () {
                     BasePageNavigator.push(CounterRoutes.welcome);
-                  }, text: Localization
-                    .of(context)
-                    .bundle
-                    .getStarted, backgroundColor: themeConfig.colors.primary1,)
-            ),
-      FlatButton(
-        onPressed: (){BasePageNavigator.push(CounterRoutes.sandbox);},
-        child: Text("sandbox", style: TextStyle(color: Colors.black),),
-      )],
+                  },
+                  text: Localization.of(context).bundle.getStarted,
+                  backgroundColor: themeConfig.colors.primary1,
+                )),
+            FlatButton(
+              onPressed: () {
+                BasePageNavigator.push(CounterRoutes.sandbox);
+              },
+              child: Text(
+                "sandbox",
+                style: TextStyle(color: Colors.black),
+              ),
+            )
+          ],
         ),
       ),
     ));
