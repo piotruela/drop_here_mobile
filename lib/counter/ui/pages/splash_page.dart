def21dc70d8c0eff436fdb97f59e2f6a87ccde4a
Piotr Maszota <pmaszota98@gmail.com>
2020-09-08 19:51:20 +0200

    Fix overflow

--- lib/counter/ui/pages/splash_page.dart
+++ lib/counter/ui/pages/splash_page.dart
@@ -18,40 +18,42 @@ class SplashPage extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 172.0),
-                child: Container(
-                    height: 193,
-                    width: 268,
-                    child: SvgPicture.asset(assetsConfig.splashScreenImage),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 102.0),
+                  child: Container(
+                      height: 193,
+                      width: 268,
+                      child: SvgPicture.asset(assetsConfig.splashScreenImage),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 19.0, bottom: 17.0),
-                child: Text(
-                    Localization.of(context).bundle.appTitle, style: themeConfig.textStyles.primaryTitle),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 67.0, right: 67.0),
-                child: Text(Localization
-                    .of(context)
-                    .bundle
-                    .lorem, style: themeConfig.textStyles.contentTitle,
-                    textAlign: TextAlign.center),
-              ),
-              Padding(//TODO: Use DhButton padding
-                  padding: const EdgeInsets.only(top: 55.0),
-                  child: DhButton(
-                    onPressed: () {
-                      Get.to(WelcomePage());
-                    }, text: Localization
+                Padding(
+                  padding: const EdgeInsets.only(top: 19.0, bottom: 17.0),
+                  child: Text(
+                      Localization.of(context).bundle.appTitle, style: themeConfig.textStyles.primaryTitle),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 67.0, right: 67.0),
+                  child: Text(Localization
                       .of(context)
                       .bundle
-                      .getStarted, backgroundColor: themeConfig.colors.primary1,)
-              )
-            ],
+                      .lorem, style: themeConfig.textStyles.contentTitle,
+                      textAlign: TextAlign.center),
+                ),
+                Padding(//TODO: Use DhButton padding
+                    padding: const EdgeInsets.only(top: 55.0),
+                    child: DhButton(
+                      onPressed: () {
+                        Get.to(WelcomePage());
+                      }, text: Localization
+                        .of(context)
+                        .bundle
+                        .getStarted, backgroundColor: themeConfig.colors.primary1,)
+                )
+              ],
+            ),
           ),
         ),
       ),
