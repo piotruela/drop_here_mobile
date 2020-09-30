7471ad57d0aef58c425a5733e81581fd8bfbb636
Piotr Maszota <pmaszota98@gmail.com>
2020-09-30 19:58:57 +0200

    Icon update

--- lib/common/ui/widgets/bottom_bar.dart
+++ lib/common/ui/widgets/bottom_bar.dart
@@ -50,7 +50,7 @@ class DHBottomBar extends StatelessWidget {
               onPressed: () => Get.to(CompanyDetailsRegistrationPage()),
               iconSize: 35.0,
               icon: Icon(
-                Icons.pin_drop_outlined,
+                Icons.pin_drop_sharp,
                 color: selectedIndex == 2
                     ? themeConfig.colors.primary1
                     : themeConfig.colors.textFieldHint,
