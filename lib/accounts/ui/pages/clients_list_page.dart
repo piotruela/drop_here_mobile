de26321444b9f30e128cda00924c7bcb724dd482
maciej.swierad <maciej.swierad@gmail.com>
2020-10-03 21:11:26 +0200

    moved circular progress indicator to column

--- lib/accounts/ui/pages/clients_list_page.dart
+++ lib/accounts/ui/pages/clients_list_page.dart
@@ -17,22 +17,70 @@ class ClientsListPage extends BlocWidget<DhListBloc> {
     dhListBloc.add(FetchClients());
     final LocaleBundle locale = Localization.of(context).bundle;
     return Scaffold(
-      backgroundColor: themeConfig.colors.background,
-      body: BlocBuilder<DhListBloc, DhListState>(
-        builder: (context, state) {
-          if (state is DhListInitial) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is ListLoading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is FetchingError) {
-            return Container(child: Text(state.error));
-          } else if (state is ClientsFetched) {
-            return buildColumnWithData(locale, state, context, dhListBloc);
-          }
-          return Container();
-        },
-      ),
-    );
+        backgroundColor: themeConfig.colors.background,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: Text(
+                  locale.persons,
+                  style: themeConfig.textStyles.primaryTitle,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  buildFlatButton(context, locale.clients, themeConfig),
+                  buildFlatButton(context, locale.sellers, themeConfig),
+                  buildFlatButton(context, locale.company, themeConfig),
+                ],
+              ),
+              DhSearchBar(dhListBloc),
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: GestureDetector(
+                  onTap: () => {dhListBloc.add(FilterClients())},
+                  child: Container(
+                    margin: const EdgeInsets.all(5.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: themeConfig.colors.addSthHere),
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.filter_list),
+                        Text(
+                          locale.filters,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              BlocBuilder<DhListBloc, DhListState>(
+                builder: (context, state) {
+                  if (state is DhListInitial) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state is ListLoading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state is FetchingError) {
+                    return Container(child: Text(state.error));
+                  } else if (state is ClientsFetched) {
+                    return buildColumnWithData(locale, state, context, dhListBloc);
+                  }
+                  return Container();
+                },
+              ),
+            ],
+          ),
+        ));
   }
 
   SafeArea buildColumnWithData(LocaleBundle locale, ClientsFetched state, BuildContext context, DhListBloc bloc) {
@@ -40,48 +88,6 @@ class ClientsListPage extends BlocWidget<DhListBloc> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Text(
-              locale.persons,
-              style: themeConfig.textStyles.primaryTitle,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(
-                width: 10.0,
-              ),
-              buildFlatButton(context, locale.clients),
-              buildFlatButton(context, locale.sellers),
-              buildFlatButton(context, locale.company),
-            ],
-          ),
-          DhSearchBar(bloc),
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: GestureDetector(
-              onTap: () => {bloc.add(FilterClients())},
-              child: Container(
-                margin: const EdgeInsets.all(5.0),
-                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
-                decoration: BoxDecoration(
-                    border: Border.all(color: themeConfig.colors.addSthHere),
-                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.filter_list),
-                    Text(
-                      locale.filters,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
           ListView.builder(
               shrinkWrap: true,
               itemCount: state.clients.length,
@@ -97,11 +103,11 @@ class ClientsListPage extends BlocWidget<DhListBloc> {
     );
   }
 
-  FlatButton buildFlatButton(BuildContext context, String text) {
+  FlatButton buildFlatButton(BuildContext context, String text, ThemeConfig themeConfig) {
     return FlatButton(
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: themeConfig.colors.white,
           borderRadius: BorderRadius.all(Radius.circular(8.0)),
         ),
         width: (MediaQuery.of(context).size.width - 130) / 3,
