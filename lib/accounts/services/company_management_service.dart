b0ca221f42cceef6c55b82b973835625646c3a6b
Piotr Maszota <pmaszota98@gmail.com>
2020-10-04 13:43:10 +0200

    Add getting products and deleting product

--- lib/accounts/services/company_management_service.dart
+++ lib/accounts/services/company_management_service.dart
@@ -7,10 +7,6 @@ import 'package:drop_here_mobile/accounts/model/api/company_management_api.dart'
 import 'package:drop_here_mobile/accounts/model/api/page_api.dart';
 import 'package:drop_here_mobile/common/data/http/http_client.dart';
 import 'package:get/get.dart';
-//TODO: Get clients list
-/*curl -X GET "https://drop-here.herokuapp.com/management/companies/customers?blocked=true
-&customerName=dd&offset=12&pageNumber=12&pageSize=12&paged=true&sort.sorted=false
-&sort.unsorted=true&unpaged=true" */
 
 class CompanyManagementService {
   final Map<String, String> _headers = {HttpHeaders.contentTypeHeader: "application/json"};
@@ -22,6 +18,13 @@ class CompanyManagementService {
     dynamic response = await _httpClient.get(
         canRepeatRequest: true, path: "/management/companies", out: (dynamic json) => json);
     return Company.fromJson(response);
+  }
+
+  Future<String> getCompanyId() async {
+    dynamic response = await _httpClient.get(
+        canRepeatRequest: true, path: "/management/companies", out: (dynamic json) => json);
+    Company companyInfo = Company.fromJson(response);
+    return companyInfo.uid;
   }
 
   Future<int> updateCompanyDetails(CompanyManagementRequest companyDetails) async {
---
+++ lib/accounts/services/product_management_service.dart
@@ -0,0 +1,29 @@
+import 'package:drop_here_mobile/accounts/model/api/company_management_api.dart';
+import 'package:drop_here_mobile/accounts/model/api/company_products_request.dart';
+import 'package:drop_here_mobile/accounts/services/company_management_service.dart';
+import 'package:drop_here_mobile/common/data/http/http_client.dart';
+import 'package:get/get.dart';
+
+class ProductManagementService {
+  final DhHttpClient _httpClient = Get.find<DhHttpClient>();
+  final CompanyManagementService _companyManagementService = Get.find<CompanyManagementService>();
+
+  Future<dynamic> getCompanyProducts(CompanyProductsRequest companyProductsRequest) async {
+    Company companyInfo = await _companyManagementService.getCompanyInfo();
+    dynamic response = await _httpClient.get(
+        canRepeatRequest: true,
+        path: "/companies/${companyInfo.uid}/products?${companyProductsRequest.toQueryParams()}",
+        out: (dynamic json) => json);
+    return response;
+    //TODO: Map response to ProductApiModel
+  }
+
+  Future<ResourceOperationResponse> deleteProduct(String productId) async {
+    String companyId = await _companyManagementService.getCompanyId();
+    dynamic response = await _httpClient.delete(
+        canRepeatRequest: true,
+        path: "/companies/$companyId/products/$productId",
+        out: (dynamic json) => json);
+    return ResourceOperationResponse.fromJson(response);
+  }
+}
