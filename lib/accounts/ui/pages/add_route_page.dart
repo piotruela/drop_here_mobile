661143c91201b8d639cdf1e6813de52e8a6c3a11
maciej.swierad <maciej.swierad@gmail.com>
2020-11-05 14:17:20 +0100

    changed  initial date in datepicker

--- lib/accounts/ui/pages/add_route_page.dart
+++ lib/accounts/ui/pages/add_route_page.dart
@@ -166,7 +166,8 @@ class AddRoutePage extends BlocWidget<AddRouteBloc> {
   void chooseDate(BuildContext context, AddRouteBloc bloc) async {
     DateTime dateTime = await showDatePicker(
         context: context,
-        initialDate: DateTime.now(),
+        initialDate: DateTime.parse(bloc.state.routeRequest.date) ?? DateTime.now(),
+        //initialDate: DateTime.now(),
         firstDate: DateTime.now(),
         lastDate: DateTime(DateTime.now().year + 10, 1, 1));
     bloc.add(FormChanged(
