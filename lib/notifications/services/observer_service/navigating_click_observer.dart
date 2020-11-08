bba032f3b4c03d8709322a55e8b7013831c3b583
Maciej Reszke <m.reszke@paymenttechnology.pl>
2020-11-08 21:24:00 +0100

    fixed bugs

--- lib/notifications/services/observer_service/navigating_click_observer.dart
+++ lib/notifications/services/observer_service/navigating_click_observer.dart
@@ -1,3 +1,4 @@
+import 'package:drop_here_mobile/app_storage/app_storage_service.dart';
 import 'package:drop_here_mobile/spots/bloc/spot_details_bloc.dart';
 import 'package:drop_here_mobile/spots/ui/pages/customer_map_page.dart';
 import 'package:get/get.dart';
@@ -5,9 +6,12 @@ import 'package:get/get.dart';
 import 'notification_observer_service.dart';
 
 class NavigatingClickObserver extends NotificationObserver {
-  //todo macias - check czy jest zalogowany jeszcze
+
   @override
   Future<void> handleNotification(NotificationPayload payload) {
+    if (!Get.find<AppStorageService>().authenticated) {
+      return Future.value();
+    }
     if (_clicked(payload)) {
       return _handleClickedNotification(payload);
     }
@@ -22,13 +26,13 @@ class NavigatingClickObserver extends NotificationObserver {
 
   Future<void> _handleSpotNotification(NotificationPayload payload) {
     return Get.to(CustomerMapPage(
-        spotDetailsOnLoadEvent: new FetchSpotDetailsEvent(spotUid: payload.referencedSubjectId)));
+        spotDetailsOnLoadEvent: new FetchSpotDetailsEvent(spotUid: payload.referencedSubjectId)), preventDuplicates: false);
   }
 
   Future<void> _handleClickedNotification(NotificationPayload payload) {
     switch (payload.referencedSubjectType) {
       case ReferencedSubjectType.DROP:
-        //todo piotruela przekierowanie na drop (np. gdy zmienił status - notyfikacje do klienta)
+      //todo piotruela przekierowanie na drop (np. gdy zmienił status - notyfikacje do klienta)
       case ReferencedSubjectType.SHIPMENT:
         //todo piotruela przekierowanie na shipment (dla firmy i klienta)
         break;
