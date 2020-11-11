dd4e5b2fefa64a09bbb67f9a7121dd2a271db638
maciej.swierad <maciej.swierad@gmail.com>
2020-11-11 10:55:17 +0100

    added model from javier

---
+++ lib/shipments/model/shipment_customer_management_api.dart
@@ -0,0 +1,75 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class ShipmentCustomerSubmissionRequest {
+  String comment;
+  List<ShipmentProductRequest> products;
+
+  ShipmentCustomerSubmissionRequest({this.comment, this.products});
+
+  ShipmentCustomerSubmissionRequest.fromJson(Map<String, dynamic> json) {
+    comment = json['comment'];
+    if (json['products'] != null) {
+      products = new List<ShipmentProductRequest>();
+      json['products'].forEach((v) {
+        products.add(new ShipmentProductRequest.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['comment'] = this.comment;
+    if (this.products != null) {
+      data['products'] = this.products.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+@JsonSerializable()
+class ShipmentProductRequest {
+  List<ShipmentCustomizationRequest> customizations;
+  int quantity;
+  int routeProductId;
+
+  ShipmentProductRequest({this.customizations, this.quantity, this.routeProductId});
+
+  ShipmentProductRequest.fromJson(Map<String, dynamic> json) {
+    if (json['customizations'] != null) {
+      customizations = new List<ShipmentCustomizationRequest>();
+      json['customizations'].forEach((v) {
+        customizations.add(new ShipmentCustomizationRequest.fromJson(v));
+      });
+    }
+    quantity = json['quantity'];
+    routeProductId = json['routeProductId'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.customizations != null) {
+      data['customizations'] = this.customizations.map((v) => v.toJson()).toList();
+    }
+    data['quantity'] = this.quantity;
+    data['routeProductId'] = this.routeProductId;
+    return data;
+  }
+}
+
+@JsonSerializable()
+class ShipmentCustomizationRequest {
+  int id;
+
+  ShipmentCustomizationRequest({this.id});
+
+  ShipmentCustomizationRequest.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    return data;
+  }
+}
