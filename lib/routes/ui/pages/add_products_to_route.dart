4095f723281c8ed36ae68bc9b414b21726727bbf
maciej.swierad <maciej.swierad@gmail.com>
2020-11-23 10:35:58 +0100

    added back buttons

--- lib/routes/ui/pages/add_products_to_route.dart
+++ lib/routes/ui/pages/add_products_to_route.dart
@@ -3,6 +3,7 @@ import 'package:drop_here_mobile/accounts/ui/widgets/dh_plain_text_form_field.da
 import 'package:drop_here_mobile/accounts/ui/widgets/dh_shadow.dart';
 import 'package:drop_here_mobile/common/config/theme_config.dart';
 import 'package:drop_here_mobile/common/ui/widgets/bloc_widget.dart';
+import 'package:drop_here_mobile/common/ui/widgets/dh_back_button.dart';
 import 'package:drop_here_mobile/common/ui/widgets/labeled_switch.dart';
 import 'package:drop_here_mobile/locale/locale_bundle.dart';
 import 'package:drop_here_mobile/locale/localization.dart';
@@ -19,7 +20,8 @@ class AddProductsToRoutePage extends BlocWidget<AddProductsToRouteBloc> {
 
   AddProductsToRoutePage({this.selectedProducts});
   @override
-  AddProductsToRouteBloc bloc() => AddProductsToRouteBloc()..add(FetchProducts(selectedProducts: selectedProducts));
+  AddProductsToRouteBloc bloc() =>
+      AddProductsToRouteBloc()..add(FetchProducts(selectedProducts: selectedProducts));
 
   @override
   Widget build(BuildContext context, AddProductsToRouteBloc bloc, _) {
@@ -32,36 +34,40 @@ class AddProductsToRoutePage extends BlocWidget<AddProductsToRouteBloc> {
         onTap: () => Get.back(result: bloc.state.selectedProducts),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              localeBundle.addProductsToRoute,
-              style: themeConfig.textStyles.primaryTitle,
-            ),
-            BlocBuilder<AddProductsToRouteBloc, AddProductsToRouteState>(
-              buildWhen: (previous, current) => previous != current,
-              builder: (context, state) {
-                if (state.type == AddProductsToRouteStateType.initial ||
-                    state.type == AddProductsToRouteStateType.loading) {
-                  return Center(child: CircularProgressIndicator());
-                } else {
-                  return Column(
-                    children: [
-                      for (ProductResponse product in bloc.state.products ?? [])
-                        AddProductToRouteCard(
-                            bloc: bloc,
-                            product: product,
-                            productRequest: bloc.state.selectedProducts
-                                .firstWhere((element) => element.productId == product.id, orElse: () => null))
-                    ],
-                  );
-                }
-              },
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DhBackButton(padding: EdgeInsets.zero,),
+              Text(
+                localeBundle.addProductsToRoute,
+                style: themeConfig.textStyles.primaryTitle,
+              ),
+              BlocBuilder<AddProductsToRouteBloc, AddProductsToRouteState>(
+                buildWhen: (previous, current) => previous != current,
+                builder: (context, state) {
+                  if (state.type == AddProductsToRouteStateType.initial ||
+                      state.type == AddProductsToRouteStateType.loading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else {
+                    return Column(
+                      children: [
+                        for (ProductResponse product in bloc.state.products ?? [])
+                          AddProductToRouteCard(
+                              bloc: bloc,
+                              product: product,
+                              productRequest: bloc.state.selectedProducts.firstWhere(
+                                  (element) => element.productId == product.id,
+                                  orElse: () => null))
+                      ],
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -241,8 +247,9 @@ class _AmountDialogState extends State<AmountDialog> {
             child: SubmitFormButton(
               text: "Submit",
               onTap: () => Navigator.pop(context, product),
-              isActive: (!_limited || (product.amount != null && product.amount.toString() != "")) &&
-                  (product.price != null && product.price.toString() != ""),
+              isActive:
+                  (!_limited || (product.amount != null && product.amount.toString() != "")) &&
+                      (product.price != null && product.price.toString() != ""),
             ),
           ),
         ],
