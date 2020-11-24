28f8f394bd6664148a66c11385a8dbf63c369c67
maciej.swierad <maciej.swierad@gmail.com>
2020-11-24 12:33:43 +0100

    added DoubleBackToCloseApp to company pages

--- lib/management/ui/pages/management_page.dart
+++ lib/management/ui/pages/management_page.dart
@@ -1,7 +1,9 @@
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:drop_here_mobile/common/config/theme_config.dart';
 import 'package:drop_here_mobile/common/thresholds.dart';
 import 'package:drop_here_mobile/common/ui/widgets/add_new_item_panel.dart';
 import 'package:drop_here_mobile/common/ui/widgets/bottom_bar.dart';
+import 'package:drop_here_mobile/common/ui/widgets/snackbar.dart';
 import 'package:drop_here_mobile/locale/locale_bundle.dart';
 import 'package:drop_here_mobile/locale/localization.dart';
 import 'package:drop_here_mobile/management/ui/pages/sellers_list_page.dart';
@@ -25,54 +27,58 @@ class ManagementPage extends StatelessWidget {
     final LocaleBundle locale = Localization.of(context).bundle;
     final double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Stack(
-        children: [
-          SafeArea(
-            child: DefaultTabController(
-              initialIndex: initialIndex,
-              length: 3,
-              child: Scaffold(
-                body: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25.0, top: 30.0, bottom: 12.0),
-                      child: Text(
-                        locale.management,
-                        style: themeConfig.textStyles.primaryTitle,
+      body: DoubleBackToCloseApp(
+        snackBar: dhSnackBar(locale.tapBackButtonAgainHint),
+        child: Stack(
+          children: [
+            SafeArea(
+              child: DefaultTabController(
+                initialIndex: initialIndex,
+                length: 3,
+                child: Scaffold(
+                  body: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0, top: 30.0, bottom: 12.0),
+                        child: Text(
+                          locale.management,
+                          style: themeConfig.textStyles.primaryTitle,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 0.0),
-                      child: TabBar(
-                        indicator: BoxDecoration(
-                            color: themeConfig.colors.white,
-                            borderRadius: BorderRadius.circular(10)),
-                        tabs: <Widget>[
-                          buildTab(locale.clients, width),
-                          buildTab(locale.sellers, width),
-                          buildTab(locale.company, width),
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0.0),
+                        child: TabBar(
+                          indicator: BoxDecoration(
+                              color: themeConfig.colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          tabs: <Widget>[
+                            buildTab(locale.clients, width),
+                            buildTab(locale.sellers, width),
+                            buildTab(locale.company, width),
+                          ],
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: TabBarView(
-                        children: [
-                          ClientsListPage(),
-                          SellersListPage(),
-                          CompanyDetailsPage(),
-                        ],
-                      ),
-                    )
-                  ],
+                      Expanded(
+                        flex: 1,
+                        child: TabBarView(
+                          children: [
+                            ClientsListPage(),
+                            SellersListPage(),
+                            CompanyDetailsPage(),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  bottomNavigationBar:
+                      CompanyBottomBar(controller: panelController, sectionIndex: 3),
                 ),
-                bottomNavigationBar: CompanyBottomBar(controller: panelController, sectionIndex: 3),
               ),
             ),
-          ),
-          AddNewItemPanel(controller: panelController)
-        ],
+            AddNewItemPanel(controller: panelController)
+          ],
+        ),
       ),
     );
   }
