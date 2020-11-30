95f665338075d9b9b5a8186097c0f8baba9ae787
Piotr Maszota <maszotap@gdansk.comarch>
2020-11-30 22:03:58 +0100

    Fix tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,6 @@
-
-
 import 'package:drop_here_mobile/common/ui/widgets/main_widget.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 
 void main() {
   testWidgets('FormPage has icons and title', (WidgetTester tester) async {
