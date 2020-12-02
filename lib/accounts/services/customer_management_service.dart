6cb9a9387d1fd618639e5cbe22c41b726f230e10
Piotr Maszota <maszotap@gdansk.comarch>
2020-12-02 14:42:28 +0100

    Fix imports 2

--- lib/accounts/services/customer_management_service.dart
+++ lib/accounts/services/customer_management_service.dart
@@ -6,23 +6,22 @@ import 'package:drop_here_mobile/accounts/model/api/company_management_api.dart'
 import 'package:drop_here_mobile/accounts/model/api/customer_management_api.dart';
 import 'package:drop_here_mobile/app_storage/app_storage_service.dart';
 import 'package:drop_here_mobile/common/data/http/http_client.dart';
-import 'package:get/get.dart';
+import 'package:get/get.dart' as di;
 
 import 'authentication_service.dart';
 
 class CustomerManagementService {
-  final DhHttpClient _httpClient = Get.find<DhHttpClient>();
-  final AppStorageService _appStorage = Get.find<AppStorageService>();
-  final AuthenticationService authenticationService = Get.find<AuthenticationService>();
+  final DhHttpClient _httpClient = di.Get.find<DhHttpClient>();
+  final AppStorageService _appStorage = di.Get.find<AppStorageService>();
+  final AuthenticationService authenticationService = di.Get.find<AuthenticationService>();
 
   Future<CustomerInfoResponse> getCustomerInfo() async {
-    dynamic response = await _httpClient.get(
-        canRepeatRequest: true, path: "/management/customers", out: (dynamic json) => json);
+    dynamic response =
+        await _httpClient.get(canRepeatRequest: true, path: "/management/customers", out: (dynamic json) => json);
     return CustomerInfoResponse.fromJson(response);
   }
 
-  Future<ResourceOperationResponse> updateCustomerInfo(
-      CustomerManagementRequest customerManagementRequest) async {
+  Future<ResourceOperationResponse> updateCustomerInfo(CustomerManagementRequest customerManagementRequest) async {
     dynamic response = await _httpClient.put(
         canRepeatRequest: true,
         path: "/management/customers",
@@ -37,8 +36,7 @@ class CustomerManagementService {
       dio.options.headers[HttpHeaders.authorizationHeader] = _appStorage.authorizationHeader;
       MultipartFile multipartFile = await MultipartFile.fromFile(file.path);
       FormData formData = FormData.fromMap({"image": multipartFile});
-      Response response =
-          await dio.post("${_httpClient.baseUrl}/management/customers/images", data: formData);
+      Response response = await dio.post("${_httpClient.baseUrl}/management/customers/images", data: formData);
       return ResourceOperationResponse.fromJson(response.data);
     } catch (error) {
       return ResourceOperationResponse()..operationStatus = OperationStatus.ERROR;
